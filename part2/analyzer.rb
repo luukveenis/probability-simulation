b351da594953f4d0e219244088c4967824c1e9d6
Luuk Veenis <luukveenis@shaw.ca>
2015-11-09 18:47:27 -0800

    Implement Naive Bayes classifier
    This classifier is built on the conditional probabilities computed
    previously. Since it assumes all attributes are conditionally
    independet, we can compute the class-conditional probability by simply
    multiplying the probabilities computed previously.
    
    This also adds an accuracy method that computes the accuracy of the
    provided labeling. This should be able to be reused for later parts
    doing cross-validation.

--- part2/analyzer.rb
+++ part2/analyzer.rb
@@ -15,4 +15,52 @@ class Analyzer
     count = @features[polarity].count.to_f
     (0..7).map { |i| @features[polarity].count { |f| f[i] } / count }
   end
+
+  # Add class labels using the Naive Bayes classifier method, using the entire
+  # data set as test data
+  def classify
+    pos_probs = cond_probs(:pos)
+    neg_probs = cond_probs(:neg)
+
+    # Label the reviews in each polarity folder
+    pos_labels = @features[:pos].map { |f| label(f, pos_probs, neg_probs) }
+    neg_labels = @features[:neg].map { |f| label(f, pos_probs, neg_probs) }
+
+    { pos_labels: pos_labels, neg_labels: neg_labels }
+  end
+
+  # Compute the accuracy of the given labeling with respect to the known
+  # classification of the reviews
+  def accuracy labels
+    pos_size = labels[:pos_labels].size
+    neg_size = labels[:neg_labels].size
+    pos_count = labels[:pos_labels].count { |l| l == :pos }
+    neg_count = labels[:neg_labels].count { |l| l == :neg }
+
+    # Compute the accuracies of the labeling
+    pos_accuracy = pos_count / pos_size.to_f
+    neg_accuracy = neg_count / neg_size.to_f
+    accuracy = (pos_count + neg_count) / (pos_size + neg_size).to_f
+
+    # Return a hash containing the results:
+    # - the overall classification accuracy
+    # - the confusion matrix for the results
+    {
+      accuracy: accuracy,
+      confusion: [[pos_accuracy, (1 - pos_accuracy)],
+                  [(1 - neg_accuracy), neg_accuracy]]
+    }
+  end
+
+  # Compute the class-conditional probability given the probabilities for
+  # each polarity and return the class label for the more likely case
+  def label vector, pos_probs, neg_probs
+    pos = vector.zip(pos_probs).reduce(1) do |acc, pair|
+      pair[0] ? acc * pair[1] : acc * (1 - pair[1])
+    end
+    neg = vector.zip(neg_probs).reduce(1) do |acc, pair|
+      pair[0] ? acc * pair[1] : acc * (1 - pair[1])
+    end
+    pos >= neg ? :pos : :neg
+  end
 end
--- part2/runner.rb
+++ part2/runner.rb
@@ -3,16 +3,22 @@
 require_relative 'parser'
 require_relative 'analyzer'
 
-def display_probs analyzer
+def display_results analyzer
   # Get and display the conditional probabilities for each polarity
   [:pos, :neg].each do |p|
     probs = analyzer.cond_probs(p)
     puts "P(Xi | #{p.to_s}) = [#{probs.join(" ")}]"
+  end
+  result = analyzer.accuracy(analyzer.classify)
+  puts "\nClassifier accuracy: " + result[:accuracy].to_s
+  puts "\nConfusion matrix:"
+  result[:confusion].each do |r|
+    puts r.join("\t")
   end
 end
 
 if __FILE__ == $0
   parser = Parser.new("./data/txt_sentoken")
   analyzer = Analyzer.new(parser.features)
-  display_probs(analyzer)
+  display_results(analyzer)
 end
